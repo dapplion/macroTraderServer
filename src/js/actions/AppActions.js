d6bb9267b7c8b2c11fa1664ae9dd2e52c5b079c9
Unknown <guillem.lion.benosa@gmail.com>
2018-04-04 18:22:52 +0200

    added public

--- src/js/actions/AppActions.js
+++
@@ -1,9 +0,0 @@
-import dispatcher from "../dispatcher";
-import AppStore from 'Store';
-
-export function updateVariable(variable) {
-  dispatcher.dispatch({
-    type: AppStore.tag.UPDATE_VARIABLE,
-    variable: variable
-  });
-}
--- src/js/params.js
+++
@@ -1,5 +0,0 @@
-// Unique location of hardcoded variables that may change in the future
-
-export default {
-  variable: 'hello world',
-};
